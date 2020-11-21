2ae77df8f2a0d524a41019818bfae06e6e3ebb9b
KristiinaKr <krausekristiina@gmail.com>
2020-11-21 19:49:46 +0200

    Started with Axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <Header :cart="cart"/>
-    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
+import Axios from "axios";
 import App from './App.vue'
 import ItemPage from "./components/ItemPage";
 import Home from "./components/Home";
