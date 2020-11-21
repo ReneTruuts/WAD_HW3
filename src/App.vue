7561de705310211132a5c681e7f51b26de4e561d
KristiinaKr <krausekristiina@gmail.com>
2020-11-21 16:03:23 +0200

    Incorporated sample logic and started working on rewriting HW2 login page to Vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Header :cart="cart"/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+
+  name: 'app',
+  methods: {
+
+  },
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: 'Open Sans', sans-serif;
+
+}
+
+html, body {
+  margin: 70px 0 0 0;
+  padding: 0;
+  background-color: lightskyblue;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="list">
+    <div v-for="(item, index) in items" :key="index">
+        <router-link to="/LoginPage">Login Page
+        <img :src="item.image">
+        <h4>
+          <span>{{item.title}} - {{item.price}}$</span>
+        </h4>
+      </router-link>
+       <LoginPage :index="index"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import LoginPage from "./LoginPage";
+
+export default {
+  name: 'Home',
+  components: {
+    LoginPage
+  },
+  computed: {
+    items: function () {
+      return this.$store.state.items
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.list {
+  display: flex;
+  flex-direction: row;
+  justify-items: center;
+}
+
+.list > div {
+  margin: 20px;
+  padding: 20px;
+  text-align: center;
+  flex: 1 1 0px;
+  background-color: #ffffff;
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px 0px #a5a5a5;;
+}
+
+.list > div img {
+  height: 100px;
+}
+
+.list > div span {
+  color: #1a1a1a;
+  text-decoration: none;
+}
+
+.list > div span:hover {
+  color: #ff5f52;
+  text-decoration: underline;
+}
+</style>
